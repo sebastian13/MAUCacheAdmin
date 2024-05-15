8be8ae5e837f17a6bda7a92322b6208fa5cc378d
Nick Ireland <nick.k.ireland@gmail.com>
2024-05-15 15:53:21 +1000

    Add filter for apps that only exist in Production channel

--- PSModule/MAUCacheAdmin/Public/Get-MAUApps.ps1
+++ PSModule/MAUCacheAdmin/Public/Get-MAUApps.ps1
@@ -22,38 +22,40 @@ function Get-MAUApps {
 
     # Define the target apps
     $targetApps = @(
-        #[PSCustomObject]@{AppID = "0409MSau03";     AppName = "MAU 3.x"}
-        [PSCustomObject]@{AppID = "0409MSau04";     AppName = "MAU 4.x"}
-        [PSCustomObject]@{AppID = "0409MSWD2019";   AppName = "Word 365/2021/2019"}
-        [PSCustomObject]@{AppID = "0409XCEL2019";   AppName = "Excel 365/2021/2019"}
-        [PSCustomObject]@{AppID = "0409PPT32019";   AppName = "PowerPoint 365/2021/2019"}
-        [PSCustomObject]@{AppID = "0409OPIM2019";   AppName = "Outlook 365/2021/2019"}
-        [PSCustomObject]@{AppID = "0409ONMC2019";   AppName = "OneNote 365/2021/2019"}
-        [PSCustomObject]@{AppID = "0409MSWD15";     AppName = "Word 2016"}
-        [PSCustomObject]@{AppID = "0409XCEL15";     AppName = "Excel 2016"}
-        [PSCustomObject]@{AppID = "0409PPT315";     AppName = "PowerPoint 2016"}
-        [PSCustomObject]@{AppID = "0409OPIM15";     AppName = "Outlook 2016"}
-        [PSCustomObject]@{AppID = "0409ONMC15";     AppName = "OneNote 2016"}
-        [PSCustomObject]@{AppID = "0409MSFB16";     AppName = "Skype for Business"}
-        [PSCustomObject]@{AppID = "0409IMCP01";     AppName = "Intune Company Portal"}
-        [PSCustomObject]@{AppID = "0409MSRD10";     AppName = "Remote Desktop v10"}
-        [PSCustomObject]@{AppID = "0409ONDR18";     AppName = "OneDrive"}
-        [PSCustomObject]@{AppID = "0409WDAV00";     AppName = "Defender ATP"}
-        [PSCustomObject]@{AppID = "0409EDGE01";     AppName = "Edge"}
-        [PSCustomObject]@{AppID = "0409TEAMS10";    AppName = "Teams 1.0 classic"}
-        [PSCustomObject]@{AppID = "0409TEAMS21";    AppName = "Teams 2.1"}
-        [PSCustomObject]@{AppID = "0409OLIC02";     AppName = "Office Licensing Helper"}
-        [PSCustomObject]@{AppID = "0409MSRH01";     AppName = "Remote Help"}
-        [PSCustomObject]@{AppID = "0409MSQA01";     AppName = "Quick Assist"}
+        #[PSCustomObject]@{AppID = "0409MSau03";     AppName = "MAU 3.x";                    Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409MSau04";     AppName = "MAU 4.x";                    Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409MSWD2019";   AppName = "Word 365/2021/2019";         Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409XCEL2019";   AppName = "Excel 365/2021/2019";        Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409PPT32019";   AppName = "PowerPoint 365/2021/2019";   Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409OPIM2019";   AppName = "Outlook 365/2021/2019";      Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409ONMC2019";   AppName = "OneNote 365/2021/2019";      Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409MSWD15";     AppName = "Word 2016";                  Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409XCEL15";     AppName = "Excel 2016";                 Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409PPT315";     AppName = "PowerPoint 2016";            Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409OPIM15";     AppName = "Outlook 2016";               Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409ONMC15";     AppName = "OneNote 2016";               Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409MSFB16";     AppName = "Skype for Business";         Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409IMCP01";     AppName = "Intune Company Portal";      Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409MSRD10";     AppName = "Remote Desktop v10";         Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409ONDR18";     AppName = "OneDrive";                   Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409WDAV00";     AppName = "Defender ATP";               Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409EDGE01";     AppName = "Edge";                       Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409TEAMS10";    AppName = "Teams 1.0 classic";          Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409TEAMS21";    AppName = "Teams 2.1";                  Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409OLIC02";     AppName = "Office Licensing Helper";    Channels = @("Production", "Preview", "Beta")}
+        [PSCustomObject]@{AppID = "0409MSRH01";     AppName = "Remote Help";                Channels = @("Production")}
+        [PSCustomObject]@{AppID = "0409MSQA01";     AppName = "Quick Assist";               Channels = @("Production")}
     )
 
+    $filteredApps = $targetApps | Where-Object {$_.Channels -contains $Channel}
+
     $pos = 1
-    $mauApps = @($targetApps | ForEach-Object {
-        Write-Progress -Id 0 -Activity "Processing Apps $pos of $($targetApps.Count)" -Status "Channel: $Channel AppID: $($_.AppID) AppName: $($_.AppName)" -PercentComplete ($pos / $targetApps.Count * 100)
+    $mauApps = @($filteredApps | ForEach-Object {
+        Write-Progress -Id 0 -Activity "Processing Apps $pos of $($filteredApps.Count)" -Status "Channel: $Channel AppID: $($_.AppID) AppName: $($_.AppName)" -PercentComplete ($pos / $filteredApps.Count * 100)
         Get-MAUApp -AppID $_.AppID -AppName $_.AppName -ChannelURI $mauCdnUriBuilder.Uri -HttpClient $httpClient
         $pos++
     })
-    Write-Progress -Id 0 -Activity "Processing Apps $pos of $($targetApps.Count)" -Completed
+    Write-Progress -Id 0 -Activity "Processing Apps $pos of $($filteredApps.Count)" -Completed
 
     $httpClient.Dispose()
 
